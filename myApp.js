558942ffbe05b125b303049ec77b2055d84fe17d
unknown <andremayanja@gmail.com>
2022-06-19 21:55:16 -0700

    changes

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
+
+app.get("/",function(req, res){
+  //res.send("Hello Express");
+  res.sendFile(__dirname+"/views/index.html");
+});
+
+app.use("/public",express.static(__dirname+"/public"));
 
 
 
